2cba91980a2070890c6b974f111f356a2e541be6
Soaif Ikbal Mondal <soaif.mondal@weavers-web.com>
2024-10-14 17:30:12 +0530

    type testing

---
+++ typeTest.dart
@@ -0,0 +1,11 @@
+
+void main(){
+  var name = "Soaif";
+  var age = 25;
+
+  print( name is int);
+  print( age is String);
+
+  print("Type testing: ${name is! int}");
+  print("Type testing: ${age is! String}");
+}
