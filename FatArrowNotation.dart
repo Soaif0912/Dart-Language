bcb11447d95356f111f7326d033ec26536ab61fa
Soaif Ikbal Mondal <soaif.mondal@weavers-web.com>
2024-10-14 17:29:37 +0530

    Fat arrow notation

---
+++ FatArrowNotation.dart
@@ -0,0 +1,12 @@
+
+void main(){
+  print(add(1,2));
+}
+
+// int add(int a, int b){
+//   int c = a + b;
+//   return c;
+// }
+
+int add(int a, int b) => a+b;
+// here add function will automaticly return a+b, this should be used only for single statement.
