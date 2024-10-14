04d76b1ddc3126899f5765a672ad2a2626e60566
Soaif Ikbal Mondal <107474186+Soaif0912@users.noreply.github.com>
2024-10-14 06:13:12 +0000

    some text change

--- dynamicVariable.dart
+++ dynamicVariable.dart
@@ -1,7 +1,7 @@
 void main() {
   dynamic a = 10;
   print(a.runtimeType);
-  // When we use dynamic keyword to create a variable we assign any datatype value to the variable and also later we can change to any datatype value.
+  // When we use dynamic keyword to create a variable, we can assign any datatype value to the variable and also later we can change to any datatype value.
 
   a = "Hello";
   print(a.runtimeType);
