219e75b8d062c729c18ec83fa3753c87494463f1
Soaif Ikbal Mondal <107474186+Soaif0912@users.noreply.github.com>
2024-10-15 14:36:50 +0000

    List method

---
+++ ListMethod.dart
@@ -0,0 +1,15 @@
+//add
+// insert(index, value)
+// List.first
+// List.end
+// remove
+// removeAt(index)
+
+// List.sort()
+// List.shuffle()
+// List.length
+// List.reversed
+// List.runtimeType
+// List.isEmpty
+// List.isNotEmpty
+// List.clear
