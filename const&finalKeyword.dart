fe4dca2073ca5b35c3194213e8b52e5aef2a7f2c
Soaif Ikbal Mondal <soaif.mondal@weavers-web.com>
2024-10-15 11:34:45 +0530

    Comment text changes

--- const&finalKeyword.dart
+++ const&finalKeyword.dart
@@ -1,7 +1,7 @@
 
 void main(){
 
-  // when we use final and const keyword to create a variable that variable value will be constant or unchangeable.
+  // When we use final and const keyword to create a variable that variable value will be constant or unchangeable.
   final v1 = "Soaif";
   const v2 = "Ikbal";
 
@@ -19,11 +19,11 @@ void main(){
 
   var foo = const []; 
  
-  foo = [1, 2, 3]; 
+  foo = [1, 2, 3];
   print(foo);
 }
 
-// variable inside class are called instance variable.
+// Variable inside class are called instance variable.
 
 class user{
   final name = "Jake";
@@ -37,12 +37,12 @@ class user{
 
   // 1.
   // When you use const, you are saying that the value never changes and is fixed at compile time.
-  // final means the value can only be set once, but it doesn’t have to be known until runtime (when the program is running).
+  // Final means the value can only be set once, but it doesn’t have to be known until runtime (when the program is running).
 
   // 2. 
   // Others variable value can be assigned to final variable.
-  // const variable must be initialized with constant value.
+  // Const variable must be initialized with constant value.
 
   // 3.
-  // only static variable can be decalered as const inside class.
-  // final variable can be decalered normally.
+  // Only static variable can be decalered as const inside class.
+  // Final variable can be decalered normally.
