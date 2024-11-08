668a8575e448dcd4e70778e94e71828b49521305
Soaif Ikbal Mondal <soaif0912@gmail.com>
2024-11-08 23:58:04 +0530

    Encapsulation

---
+++ classes/encapsulation.dart
@@ -0,0 +1,34 @@
+class BankAccount {
+  double _balance = 0.0; // Private variable
+
+  BankAccount(double initialBalance) {
+    _balance = initialBalance;
+  }
+
+  void deposit(double amount) {
+    _balance += amount;
+  }
+
+  double getBalance() {
+    return _balance; // Indirect access to the private variable
+  }
+}
+
+main() {
+  var myBank = BankAccount(2000000.00);
+  print(myBank.getBalance());
+}
+
+
+// How Encapsulation Works in Dart
+// In Dart, encapsulation is typically implemented using private variables and methods and controlled access to those variables with getter and setter methods.
+
+// Private Variables
+// In Dart, any variable or method starting with an underscore _ is considered private to the library it is defined in. This means other files or parts of the program cannot access it directly.
+
+
+// Here:
+
+// _balance is private because it starts with an underscore _.
+// We can't directly access _balance outside the BankAccount class.
+// Instead, we use a method getBalance() to access it indirectly.
