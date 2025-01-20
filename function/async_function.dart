2b3cf0eae99af37f5eb6d579fdf6eb2b657f8409
Soaif Ikbal Mondal <107474186+Soaif0912@users.noreply.github.com>
2025-01-20 11:34:24 +0000

    async function

---
+++ function/async_function.dart
@@ -0,0 +1,13 @@
+import 'typeDef.dart';
+
+Future<void> profileName() async {
+  await Future.delayed(Duration(seconds:2));
+  print("Function completed");
+}
+
+void main() {
+  profileName();
+  print('I am first');
+}
+
+// Calling fetchData() will wait 2 seconds and then print "Data fetched".<Void> means The function does not return any data.
