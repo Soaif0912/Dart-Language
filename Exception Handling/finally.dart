a2e4df86556945bd39e93d70e7f78c4d45801f15
Soaif Ikbal Mondal <107474186+Soaif0912@users.noreply.github.com>
2025-01-22 10:00:59 +0000

    finaly block

--- Exception Handling/finally.dart
+++ Exception Handling/finally.dart
@@ -11,4 +11,28 @@ void main() {
   } finally {
     print('This code runs regardless of exception.');
   }
+
+  try {
+    print('Opening file...');
+    throw Exception('An error occurred!'); // Simulating an error
+  } catch (e) {
+    print('Caught exception: $e');
+  } finally {
+    print('Closing file...');
+  }
+
+  try {
+    print('Opening file...');
+    print('File operation successful.');
+  } catch (e) {
+    print('Caught exception: $e');
+  } finally {
+    print('Closing file...');
+  }
+
 }
+
+// Key Points:
+// The finally block always runs after try and catch, regardless of whether an exception occurred.
+// Use finally for cleanup tasks like closing files, network connections, or freeing resources.
+// Omitting resource cleanup (like closing the client) can lead to memory or resource leaks.
