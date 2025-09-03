e814fac2dbd5a9b6bbcba8b5232122674e5726e6
Soaif Ikbal Mondal <soaif0912@gmail.com>
2025-09-04 00:32:07 +0530

    .

--- classes/class_object.dart
+++ classes/class_object.dart
@@ -1,4 +1,3 @@
-
 class Car {
   // Properties
   String brand;
@@ -14,7 +13,6 @@ class Car {
   }
 }
 
-
 // void main() {
 //   // Creating instances (objects) of the Car class
 //   Car car1 = Car('Toyota', 'Corolla', 2020);
@@ -25,19 +23,33 @@ class Car {
 //   car2.displayInfo(); // Output: Car: Honda Civic, Year: 2019
 // }
 
-
 class ElectricCar extends Car {
   int batteryCapacity;
 
-  ElectricCar(String brand, String model, int year, { required this.batteryCapacity}) : super(brand, model, year);
+  ElectricCar(String brand, String model, int year,
+      {required this.batteryCapacity})
+      : super(brand, model, year);
 
   void displayBattery() {
     print('Battery Capacity: $batteryCapacity kWh');
   }
 }
 
+class Demo1 {
+  int num;
+  String name;
+  Demo1({required this.num, required this.name});
+}
+
+class Demo2 extends Demo1 {
+  String address;
+  Demo2({required int num, required String name, required this.address})
+      : super(num: num, name: name);
+}
+
 void main() {
-  ElectricCar tesla = ElectricCar('Tesla', 'Model S', 2021, batteryCapacity: 100);
-  tesla.displayInfo();      // Output: Car: Tesla Model S, Year: 2021
-  tesla.displayBattery();    // Output: Battery Capacity: 100 kWh
+  ElectricCar tesla =
+      ElectricCar('Tesla', 'Model S', 2021, batteryCapacity: 100);
+  tesla.displayInfo(); // Output: Car: Tesla Model S, Year: 2021
+  tesla.displayBattery(); // Output: Battery Capacity: 100 kWh
 }
